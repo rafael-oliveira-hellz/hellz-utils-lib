142e6100d9ab721e920b969720aab503e4bf1ab8
Rafael de Oliveira <rafael.oliveira@infocaltechnologies.com>
2024-07-19 08:28:30 -0300

    chore: adds receive-pix-transaction method

--- src/wallet/index.ts
+++ src/wallet/index.ts
@@ -4,3 +4,4 @@ export * from "./check-balance";
 export * from "./transfer-value";
 export * from "./transfer-value-to-bank-account";
 export * from "./transfer-value-to-user-wallet";
+export * from "./receive-pix-transaction";
